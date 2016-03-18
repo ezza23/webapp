d5b71f673e89174b587f86eb30615368541cffda
Ezza <user@users-MacBook-Pro.local>
2016-03-18 15:59:38 +0200

    clean the code and last touches

--- js/main.js
+++ js/main.js
@@ -8,33 +8,7 @@ function all (selector) {
   return document.querySelectorAll(selector);
 }
 
-
-// update the frame according to the selected option
-function updateFrame(){
- 
-  $('#quick-reports-links').addEventListener('change',setQrFrameExpandURL);
- $('#my-team-folders-links').addEventListener('change',setTfFrameURL);
-}
-// set quick-reports  relevant link for the expand button and frame section
- function setQrFrameExpandURL() {
-
-     var el=document.getElementById("quick-reports-links");
-
-  $('#quickreports-expand-url').href=el.options[el.selectedIndex].value;
-  $('#quickreports-frame').src=el.options[el.selectedIndex].value;
-    return false;
-
-}
-// set team folders the relevant link for the expand button and frame section
- function setTfFrameURL() {
-
-     var el=document.getElementById("my-team-folders-links");
-     if(el.options!==undefined)
-  $('#tfolders-frame').src=el.options[el.selectedIndex].value;
-    return false;
-
-}
-
+// EVENT handling
 var addEventListener = function(obj, evt, fnc) {
 if (document.addEventListener) {
     var addEvent = function(elem, type, handler) {
@@ -43,21 +17,22 @@ if (document.addEventListener) {
     var removeEvent = function(elem, type, handler) {
         elem.removeEventListener(type, handler, false)
     }
-} else {
+  } else {
     var addEvent = function(elem, type, handler) {
         elem.attachEvent("on" + type, handler)
     }
     var removeEvent = function(elem, type, handler) {
         elem.detachEvent("on" + type, handler)
     }
-}
+  }
 
 };
 
 /// load data for nav-sections from JSON file
 var fillMenu=function(data){
+  // get the quick action data from json file
   var quickActions=data.quickActions;
- 
+  // select all navs sections
   var navSections = all(".nav-section");
   for (var i = 0; i < navSections.length; i++) {
     // set header for every nav-section
@@ -66,7 +41,6 @@ var fillMenu=function(data){
     navSections[i].style.background = "black url(./img/icons/" + quickActions[i].icon + ".png)  left 50% top 70px no-repeat";
   }
 
- 
   var menuCaptions = all(".menu-caption");
   for (var i = 0; i < menuCaptions.length; i++) {
     // menu header
@@ -86,12 +60,9 @@ var fillMenu=function(data){
 
 // activate the pressed tab
 var setActiveTab=function(){
-  
-
   var tabsList =document.getElementsByClassName("tabs-links");
   // current tab hash
    var active=this.hash || window.location.hash;
-
     for (var i = 0; i < tabsList.length; i++) {
         if (tabsList[i].hash == active) {
           x=tabsList[i].parentNode;
@@ -124,36 +95,54 @@ TabsNavigation= function(e) {
     switch (e.keyCode) {
         case 37:{
                 if(parseInt(curr[3])>1){
-               
-                 curr=curr.replace(curr[3],parseInt(curr[3]) - 1);
-              
-                window.location.hash = $('#'+curr).children[0].rel;
-             
-                setActiveTab();
+                  curr=curr.replace(curr[3],parseInt(curr[3]) - 1);
+                  window.location.hash = $('#'+curr).children[0].rel;
+                  setActiveTab();
                 }
             break;
             }
         case 39:{
                 if(parseInt(curr[3]) < tabsList.length){
-                curr=curr.replace(curr[3],parseInt(curr[3]) + 1);
-                window.location.hash = $('#'+curr).children[0].rel;
-                setActiveTab();
+                  curr=curr.replace(curr[3],parseInt(curr[3]) + 1);
+                  window.location.hash = $('#'+curr).children[0].rel;
+                  setActiveTab();
                 }
             break;
             }
        }
-        }
+}
+// update the frame according to the selected option
+function updateFrame(){
+ 
+  $('#quick-reports-links').addEventListener('change',setQrFrameExpandURL);
+  $('#my-team-folders-links').addEventListener('change',setTfFrameURL);
+}
+// set quick-reports  relevant link for the expand button and frame section
+ function setQrFrameExpandURL() {
+
+  var el=document.getElementById("quick-reports-links");
+  $('#quickreports-expand-url').href=el.options[el.selectedIndex].value;
+  $('#quickreports-frame').src=el.options[el.selectedIndex].value;
+    return false;
+}
+// set team folders the relevant link for the expand button and frame section
+ function setTfFrameURL() {
+
+     var el=document.getElementById("my-team-folders-links");
+     if(el.options!==undefined)
+  $('#tfolders-frame').src=el.options[el.selectedIndex].value;
+    return false;
+}
+
 
 // check the validity of the entered inputs
 var checkInputs=function(e){
-
   valid=1;
   currentTab=this.id;
   currentTab=currentTab.substr(0,currentTab.indexOf('-save'));
   var inputFields=all('#'+currentTab+'-feildset-form' +' .report-input');
   for (var i = 0; i < inputFields.length; i +=2) {
      if((inputFields[i].value!=="" && inputFields[i].value!==null) && (inputFields[i+1].value==="" || inputFields[i+1].value===null)){
-
         inputFields[i+1].required=true;
         inputFields[i].required=true;
         errorFocus(inputFields[i+1].id);
@@ -172,7 +161,7 @@ var checkInputs=function(e){
             valid=1;
       }
   }
- 
+ // if all inputs are OK then submit the form data
   if(valid==1 && $('#'+currentTab+'-feildset-form' +' .setting-form').checkValidity())
   {
     submitFormData(currentTab,inputFields);
@@ -198,28 +187,29 @@ var errorFocus=function(id){
 
 /// create localStorage and save the data from the form in it
 var createLocalStorage = function(){
-
    localStorage.clear();
     var dataArr=[];
     var forms=all('.feildset-form');
-  // check localStorage support
- if (localStorage) {
-  for(j=0; j<2; j++){
-    formid=forms[j].id;
-    thisForm=formid.substr(0,formid.indexOf('-feildset-form'));
-     var inputs=all('#'+thisForm+'-feildset-form' +' .report-input')
-   for (var i = 0; i < inputs.length; i+=2) {
-         if(inputs[i].value!==null && inputs[i].value!==""){
-         dataArr.push({
-                    name: inputs[i].value,
-                    nameKey: inputs[i].id,
-                    url: inputs[i+1].value,
-                    urlKey : inputs[i+1].id
-                });
-        }
+     // check localStorage support
+    if (localStorage) {
+     for(j=0; j<2; j++){
+      formid=forms[j].id;
+      thisForm=formid.substr(0,formid.indexOf('-feildset-form'));
+      var inputs=all('#'+thisForm+'-feildset-form' +' .report-input');
+      // save data in an array
+      for (var i = 0; i < inputs.length; i+=2) {
+           if(inputs[i].value!==null && inputs[i].value!==""){
+                 dataArr.push({
+                      name: inputs[i].value,
+                      nameKey: inputs[i].id,
+                      url: inputs[i+1].value,
+                      urlKey : inputs[i+1].id
+                  });
+            }
+        };
     };
-  };
-    localStorage.setItem('formData', JSON.stringify(dataArr));   
+    // add the data array to the local Storage
+  localStorage.setItem('formData', JSON.stringify(dataArr));   
  }
 else{
      console.log('ERROR: localStorage is not supported');
@@ -229,7 +219,6 @@ return;
 
 // submit from data to dropdown list and local storage
 var submitFormData=function(currentTab,inputs){
-
   // list of the links
   var linkList=$('#'+currentTab+'-links');
   // check if the list is empty
@@ -253,6 +242,7 @@ var submitFormData=function(currentTab,inputs){
       // if there are reports
     if(linkList.length!==0){
       // update the frame and the expand icon url
+      $('#'+currentTab +'-feildset-form').style.marginTop="14px";
       if(currentTab==="quick-reports"){
        setQrFrameExpandURL();
        // show expand icon
@@ -270,6 +260,7 @@ var submitFormData=function(currentTab,inputs){
   
   else{
   // if the frame is empty.. // hide everything (revert to the initial state)
+   $('#'+currentTab +'-feildset-form').style.marginTop="35px";
     $('#'+currentTab+'-links').classList.add('hidden');
     $('#'+currentTab +' .frame-content').classList.add('hidden');
     $('.expand').classList.add('hidden');
@@ -329,62 +320,62 @@ for (var i = 0; i < inputs.length; i++) {
     });
 };
 
-// on every tab click
-var tabs =document.getElementsByClassName("tabs-links");
 
-for (var i = 0; i < tabs.length; i++) {
-    tabs[i].addEventListener("click", setActiveTab);
-  };
-// on save button click
-document.getElementById("quick-reports-save").addEventListener('click',checkInputs);
-document.getElementById("my-team-folders-save").addEventListener('click',checkInputs);
 function initialize () {
+      // on every tab click
+      var tabs =document.getElementsByClassName("tabs-links");
+      for (var i = 0; i < tabs.length; i++) {
+          tabs[i].addEventListener("click", setActiveTab);
+        };
+      // on save button click
+      document.getElementById("quick-reports-save").addEventListener('click',checkInputs);
+      document.getElementById("my-team-folders-save").addEventListener('click',checkInputs);
+        // quick-reports setting button
+        document.getElementById("quick-reports-setting").addEventListener('click',function(e){
+          // set the setting to be active (white background)
+        $("#quick-reports-setting").classList.toggle('active-setting') ;
+          // show the feildset content
+          $("#quick-reports-feildset-form").classList.toggle('hidden');
 
-  // quick-reports setting button
-  document.getElementById("quick-reports-setting").addEventListener('click',function(e){
-    // set the setting to be active (white background)
-  $("#quick-reports-setting").classList.toggle('active-setting') ;
-    // show the feildset content
-    $("#quick-reports-feildset-form").classList.toggle('hidden');
-     document.getElementById("report1").focus();
+           document.getElementById("report1").focus();
 
-});
-  // my-team-folders setting button
-document.getElementById("my-team-folders-setting").addEventListener('click',function(e){
-  // set the setting to be active (white background)
-  $("#my-team-folders-setting").classList.toggle('active-setting') ;
-   // show the feildset content
-    $("#my-team-folders-feildset-form").classList.toggle('hidden');
-     document.getElementById("folder1").focus();
+      });
+        // my-team-folders setting button
+      document.getElementById("my-team-folders-setting").addEventListener('click',function(e){
+        // set the setting to be active (white background)
+        $("#my-team-folders-setting").classList.toggle('active-setting') ;
+         // show the feildset content
+          $("#my-team-folders-feildset-form").classList.toggle('hidden');
+           document.getElementById("folder1").focus();
 
-});
+      });
 
-// quick reports setting cancel 
-document.getElementById("quickreports-cancel").addEventListener('click',function(e){
-   e.preventDefault();
-$("#quick-reports-setting").classList.remove('active-setting') ;
-  $("#quick-reports-feildset-form").classList.add('hidden');
-   // $('#quick-reports-setting').click();
+      // quick reports setting cancel 
+      document.getElementById("quickreports-cancel").addEventListener('click',function(e){
+         e.preventDefault();
+      $("#quick-reports-setting").classList.remove('active-setting') ;
+        $("#quick-reports-feildset-form").classList.add('hidden');
+         // $('#quick-reports-setting').click();
 
-});
-// my team folders setting cancel 
-document.getElementById("teamfolders-cancel").addEventListener('click',function(e){
-   e.preventDefault();
-   $("#my-team-folders-setting").classList.remove('active-setting') ;
-     $("#my-team-folders-feildset-form").classList.add('hidden');
-   // $('#my-team-folders-setting').click();
+      });
+      // my team folders setting cancel 
+      document.getElementById("teamfolders-cancel").addEventListener('click',function(e){
+         e.preventDefault();
+         $("#my-team-folders-setting").classList.remove('active-setting') ;
+           $("#my-team-folders-feildset-form").classList.add('hidden');
+         // $('#my-team-folders-setting').click();
 
-});
-if(window.location.hash)
-setActiveTab();
-loadDataFromStorage();
+      });
+      if(window.location.hash)
+      setActiveTab();
+      loadDataFromStorage();
 
-// get notifications
- UTILS.getDataRequest();
- UTILS.ajax("data/config.json", {done: fillMenu});
-// document.onkeydown = TabsNavigation();
-document.addEventListener("keydown",TabsNavigation);
- };
+      // get notifications
+       UTILS.getDataRequest();
+       UTILS.ajax("data/config.json", {done: fillMenu});
+      // document.onkeydown = TabsNavigation();
+      document.addEventListener("keydown",TabsNavigation);
+};
 
 window.onLoad = initialize();
 
